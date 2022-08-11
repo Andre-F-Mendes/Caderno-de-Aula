056165bbfbecf29591e7487ddd4045b487b99431
André M <andre2016mendes@gmail.com>
2022-08-11 10:40:21 -0300

    Create TestaGaragem.java

---
+++ TestaGaragem.java
@@ -0,0 +1,20 @@
+public class TestaGaragem {
+
+	public static void main(String[] args) {
+		Carro clio = new Carro("Clio Autenthic");
+		Carro argo = new Carro("Argo");
+		Carro ka = new Carro("Ka");
+		
+		Garagem gar = new Garagem("garagem legal", "rua deis! num 10", 5);
+		
+		gar.estaciona(clio);
+		gar.estaciona(argo);
+		gar.estaciona(ka);
+		
+        //esse
+		Carro estacionado = gar.retira(1);
+		gar.estaciona(new Carro("Fusca"));
+		System.out.println(gar.getNome());
+	}
+
+}
