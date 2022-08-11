aa7ff43f64f94ad0501a9036614abdd8281ccdd3
André M <andre2016mendes@gmail.com>
2022-08-11 10:40:18 -0300

    Update Garagem.java
    q

--- Garagem.java
+++ Garagem.java
@@ -1,10 +1,11 @@
 public class Garagem{
-
+    
     private final String nome;
     private final String endereco;
     private final int qtdeVagas;
     private final Carro [] carrosEstacionados;
-    private int qtEstacionados = 0;
+    private int qtdeCarros = 0;
+
     public Garagem(String nome, String endereco, int qtdeVagas){
         this.nome = nome;
         this.endereco = endereco;
@@ -26,11 +27,39 @@ estacionados - assim como você utilizou a quantidade de notas de um Estudante, 
 anterior); Retorna true se conseguiu estacionar e false caso não tenha conseguido estacionar o carro
 (por exemplo quando a garagem está cheia ou o carro não está parado e desligado)
  */
-    public void estaciona(Carro c){
-
-        if(c.isLigado() == false && c.getvelocidade() == 0){
-            this.carrosEstacionados[this.qtEstacionados] = c;
+    public boolean estaciona(Carro c){
+        if(this.qtdeCarros>=qtdeVagas && c.isLigado() == false && c.getvelocidade() == 0){
+            this.carrosEstacionados[this.qtdeCarros] = c;
+            this.qtdeCarros++;
+            return true;
+        }else {
+            return false;
         }
+    }
+    /* -retira(int pos): retira um carro do estacionamento na posição (do vetor) passada por parâmetro.
+Deve retornar o carro retirado do vetor ou null caso não tenha carro na posiao passada por
+parâmetro.
+ */
+    public Carro retira(int pos){
+        //criando uma variavel tipo Carro e lançando um throw deixa de ser necessario dois if
+        Carro retorno = null;
+
+        //nao precisa mais se preocupar com os resultados invalidos
+        if(pos> 0 || pos>=this.qtdeCarros){
+            throw new IllegalArgumentException("posição inválida");
+        }
+        //retorno vai ser o valor do vetor, se for null, sera null e se for um objeto carro, tera suas informações
+        retorno = this.carrosEstacionados[pos];
+        //daqui pra baixo eu nao entendi
+        for(int i = pos+1; i<this.qtdeCarros; i++){
+            this.carrosEstacionados[i - 1] = this.carrosEstacionados[i]; 
+        }//
         
+        this.qtdeCarros--;
+        return retorno;
+    }
+    /* -getVagas(): retorna quantas vagas ainda existem disponíveis no estacionamento  */
+    public int getVagas(){
+        return this.qtdeVagas - qtdeCarros;
     }
 }
