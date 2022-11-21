59a3f5e6e2c522167613926e66fb9f07431eaed0
André M <101641732+RklmpAndre@users.noreply.github.com>
2022-11-21 02:30:10 -0300

    commit

--- java/MenuRPG/Mago.java
+++ java/MenuRPG/Mago.java
@@ -3,16 +3,26 @@ package MenuRPG;
 import java.util.ArrayList;
 
 public class Mago extends Pessoa {
+    private String talentosMago = "Adaptabilidade BarreiraMística CantoMonástico ConhecimentoMágico ContraMágica DominarMagia EscudodaFé EscreverPergaminhos Familiar FocoemMagia MagiaCamuflada MagiadosBecos MagiaNatural MagiaCombatente MédicoDeCampo PoderDoRebanho PoderMágico PrepararPoções ResíduoMágicoCurativo SacerdoteCopista TeseArcana Metamagia AcelerarMagia AmpliarMagia AumentarMagia ElevarMagia EsculpirMagia EstenderMagia InvocaçãoAberrante MagiaCongelante MagiaConspurcada MagiaCorrosiva MagiaEletrizante MagiaExplosiva MagiaPrimordial MagiaSantificada MagiasemGestos MagiaSilenciosa MaximizarMagia PotencializarMagia ToqueLongínquo");
+
+    
 
     public Mago(String nomeJogador, String nomePersonagem, String tamanho, int idade, int nivel) {
         super(nomeJogador, nomePersonagem, tamanho, idade, nivel);
     }
 
+
+
     @Override
-    public ArrayList listaTalentos() {
+    public void addTalentosClasse() {
+        for(String l : talentosMago.split(" ")){
+            super.setTalentosClasse(l);
+        }
         
-        return null;
     }
+
+    
+
 
     
     
--- java/MenuRPG/Pessoa.java
+++ java/MenuRPG/Pessoa.java
@@ -8,8 +8,11 @@ public abstract class Pessoa {
     private final String nomeJogador, nomePersonagem;
     private int idade = 0, nivel = 0;
     private String tamanho;
-    private ArrayList talentos = new ArrayList<String>();
+    private ArrayList<String> talentos = new ArrayList<>();
+    private ArrayList<String> talentosClasse = new ArrayList<String>();
+    
 
+    
     public Pessoa(String nomeJogador, String nomePersonagem, String tamanho, int idade, int nivel){
         this.nomeJogador = Objects.requireNonNull(nomeJogador, "Seu nome não pode ser nulo");
         this.nomePersonagem = Objects.requireNonNull(nomePersonagem, "Nome do personagem não pode ser nulo");
@@ -34,6 +37,24 @@ public abstract class Pessoa {
     public String getTamanho() {
         return this.tamanho;
     }
-    public abstract ArrayList listaTalentos();
+    public String getTalentos(){
+        String talento = " ";
+        for(int i = 0; i < this.talentos.size(); i++ ){
+            talento = this.talentos.get(i);
+        }
+        return talento;
+    }
+    public ArrayList<String> getTalentosClasse() {
+        return talentosClasse;
+    }
+    public void setTalentosClasse(String ) {
+        this.talentosClasse = talentosClasse;
+    }
+    public void addTalento(int pos){
+        String posTalento = this.talentosClasse.get(pos);
+
+        this.talentos.add(posTalento);
+    }
+    public abstract void addTalentosClasse();
 }
   
