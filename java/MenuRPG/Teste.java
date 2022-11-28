f4d674df5cea0673db0f75571c3229cb322815c0
André M <101641732+RklmpAndre@users.noreply.github.com>
2022-11-28 11:06:22 -0300

    commit

--- java/MenuRPG/Teste.java
+++ java/MenuRPG/Teste.java
@@ -5,25 +5,25 @@ public class Teste {
     public static void main(String[] args) {
         double[] habilidadesAndre = {1.0, 12.0, 11.0, 1.0, 20.0, 17.0};
 
-        Pessoa andre = new Mago("André", "Zapson", "MEDIO", 23, 5);  
+        Pessoa andre = new Mago("André", "Zapson", "MEDIO", 23, 19);  
         
-        System.out.println("Jogador: " + andre.getNomeJogador());
-        System.out.println("Personagem: " + andre.getNomePersonagem());
-        System.out.println("Tamanho: " + andre.getTamanho());
-        System.out.println("Idade: " + andre.getIdade());
-        System.out.println("Nível: " + andre.getNivel());
+        // System.out.println("Jogador: " + andre.getNomeJogador());
+        // System.out.println("Personagem: " + andre.getNomePersonagem());
+        // System.out.println("Tamanho: " + andre.getTamanho());
+        // System.out.println("Idade: " + andre.getIdade());
+        // System.out.println("Nível: " + andre.getNivel());
         
-        System.out.println("Atuais Habilidades:" + andre.getHabilidades() + "\nAtuais Modificadores: ");
-        System.out.println(andre.getModificadores());
+        // System.out.println("Atuais Habilidades:" + andre.getHabilidades() + "\nAtuais Modificadores: ");
+        // System.out.println(andre.getModificadores());
         
         andre.setHabilidades(habilidadesAndre);
 
-        System.out.print("Novas Habilidades: " + andre.getHabilidades() + "\nNovos Modificadores: " + andre.getModificadores());
+        // System.out.print("Novas Habilidades: " + andre.getHabilidades() + "\nNovos Modificadores: " + andre.getModificadores());
         
         andre.fazAniversario();
         andre.upNivel();
         
-        System.out.print("\nNovas Habilidades: " + andre.getHabilidades() + "\nNovos Modificadores: " + andre.getModificadores());
+        // System.out.print("\nNovas Habilidades: " + andre.getHabilidades() + "\nNovos Modificadores: " + andre.getModificadores());
 
         // System.out.print("Nova Idade: "  + andre.getIdade() + "\nNovo Nível: ");
         // System.out.println(andre.getNivel());
@@ -38,7 +38,6 @@ public class Teste {
         andre.addItem(0);
         andre.addItem(0);
         
-
         // System.out.println("Novos Itens: " + andre.getItens() + "\nItem Especifico: ");
         // System.out.println(andre.getItem(0));
 
