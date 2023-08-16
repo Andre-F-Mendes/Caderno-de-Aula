5d620cf9c73a3ce12cfdc70b3b6b0b64a54cc9f2
RklmpAndre <andre2016mendes@gmail.com>
2023-08-16 14:24:13 -0300

    q

---
+++ Web/js/Exercicio.js
@@ -0,0 +1,23 @@
+console.log(23 + 97);
+
+console.log(10 + 30 + 90 + 20 + 67);
+
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+
+console.log(a);
+
+console.log(9 * a );
+
+let b = 7 * a
+
+console.log(b);
+
+const max = 57
+
+let actual = max - 13
+
+let porcentage = actual/max
+
+console.log(porcentage);
