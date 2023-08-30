ee4cc82f0babe8190465200795d0fa4ba5ad6c2d
RklmpAndre <andre2016mendes@gmail.com>
2023-08-30 14:59:45 -0300

    q

--- Web/js/Exercicio2.js
+++ Web/js/Exercicio2.js
@@ -1,19 +1,16 @@
-// Pergunta a idade da pessoa
-var idade = parseInt(prompt("Qual é a sua idade?"));
+let idade = 0;
 
-// Verifica a idade usando condicionais
+
 if (idade < 16) {
-
-
     console.log("Não pode entrar no evento.");
+
 } else if (idade >= 16 && idade < 18) {
-
-
     console.log("Pode entrar no evento, porém não pode beber.");
+
 } 
 else {
     console.log("Pode entrar no evento e beber.");
+    
 }
 
-// Mostra a mensagem final
 console.log("Se beber, não dirija!");
